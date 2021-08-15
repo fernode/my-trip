9aedbe964b8cf93076c0cd8e39d6355bb7244bf1
Fernando Silva <fernandosilva@MacBook-Air-de-Fernando.local>
2021-08-14 22:27:26 -0300

    feat - styles in places page

--- src/templates/places/index.tsx
+++ src/templates/places/index.tsx
@@ -1,6 +1,8 @@
+import Image from 'next/image'
 import { CloseOutline } from '@styled-icons/evaicons-outline/CloseOutline'
 import * as S from './styles'
 import LinkWrapper from 'components/LinksWrapper'
+import { useRouter } from 'next/dist/client/router'
 
 type ImageProps = {
   url: string
@@ -20,15 +22,37 @@ export type PlacesTemplateProps = {
 }
 
 const PlacesTemplate = ({ place }: PlacesTemplateProps) => {
+  const router = useRouter()
+
+  if (router.isFallback) return null
+
   return (
-    <S.Content>
+    <>
       <LinkWrapper href="/">
-        <CloseOutline size={32} />
+        <CloseOutline size={32} aria-label="Go back to map" />
       </LinkWrapper>
 
-      <h1>{place.name}</h1>
-      <div dangerouslySetInnerHTML={{ __html: place.description.html }} />
-    </S.Content>
+      <S.Wrapper>
+        <S.Container>
+          <S.Heading>{place.name}</S.Heading>
+          <S.Body
+            dangerouslySetInnerHTML={{ __html: place.description.html }}
+          />
+          <S.Gallery>
+            {place.gallery.map((image, index) => (
+              <Image
+                key={`photo-${index}`}
+                src={image.url}
+                alt={place.name}
+                width={1000}
+                height={600}
+                quality={75}
+              />
+            ))}
+          </S.Gallery>
+        </S.Container>
+      </S.Wrapper>
+    </>
   )
 }
 
